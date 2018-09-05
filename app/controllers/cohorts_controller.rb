d9cbc2d14027c69c58ec07cca4f26aa1e1b39204
viestur8 <twotigers@vivaldi.net>
2018-09-04 21:47:24 -0400

    Destroy added

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -3,4 +3,50 @@ class CohortsController < ApplicationController
   def index
     @cohorts = Cohort.all
 end
+def show
+      @cohort = Cohort.find(params[:id])
+
+  end
+
+  def new
+      @cohort = Cohort.new
+      # displays signup form
+      #   with all fields for user fill in
+  end
+
+  def edit
+      @cohort = Cohort.find(params[:id])
+
+  end
+
+  def create
+      @cohort = Cohort.create(cohort_params)
+      if @cohort.save
+          redirect_to cohorts_path
+      else
+          render 'new'
+      end
+  end
+
+  def update
+      @cohort = Cohort.find(params[:id])
+      @cohort.update(cohort_params)
+      redirect_to "/cohorts"
+  end
+
+
+  def destroy
+      @cohort = Cohort.find(params[:id])
+      @cohort.destroy
+
+      redirect_to cohorts_path
+  end
+
+  private
+  def cohort_params
+      params.require(:cohort).permit(:title, :start_date, :end_date, :students_in_cohort, :course_id, :teacher_id)
+  end
+      def set_cohort
+        @cohort = Cohort.find(params[:id])
+      end
 end
--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,10 +4,8 @@ class CoursesController < ApplicationController
     @courses = Course.all
   end
 
-
   def show
         @course = Course.find(params[:id])
-
     end
 
     def new
@@ -18,7 +16,6 @@ class CoursesController < ApplicationController
 
     def edit
         @course = Course.find(params[:id])
-
     end
 
     def create
@@ -34,17 +31,13 @@ class CoursesController < ApplicationController
         @course = Course.find(params[:id])
         @courses = Course.all
 
-        if @course.update(course_params)
-            redirect_to @courses
-        else
-            render 'edit'
-        end
+         @course.update(course_params)
+            redirect_to "/courses"
     end
 
     def destroy
         @course = Course.find(params[:id])
         @course.destroy
-
         redirect_to courses_path
     end
 
