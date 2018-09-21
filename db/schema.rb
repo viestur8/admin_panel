c533a3ecf4dcf2aacffdfc400b5aa98aa31138ef
viestur8 <twotigers@vivaldi.net>
2018-09-21 13:53:46 -0400

    Added UsersController and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_192825) do
+ActiveRecord::Schema.define(version: 2018_09_21_173646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,13 @@ ActiveRecord::Schema.define(version: 2018_08_27_192825) do
     t.integer "age"
     t.integer "salary"
     t.integer "highest_edu"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
